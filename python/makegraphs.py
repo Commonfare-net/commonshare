00e878e2cf12754ce758dda34b05cf7a9511e946
DanielRough <djr53@st-andrews.ac.uk>
2019-03-01 15:10:34 +0000

    Changed recommender data filepath

--- python/makegraphs.py
+++ python/makegraphs.py
@@ -292,7 +292,7 @@ def make_recommender_data(G,window,tag_edges):
     root[0].set('start',cf.to_str(window[0]))
     root[0].set('end',cf.to_str(window[1]))
     root[0].set('timeformat', 'date') 
-    tree.write("../data/output/recommenderdata.gexf")      
+    tree.write("data/output/recommenderdata.gexf")      
     os.remove("newdata.gexf")
 
 def jaccard(front,stepcommunity):
